acd9c8915040bd0cb1886a23be1a180ffb82780d
AustinPotts <pottsevolvements@gmail.com>
2021-12-16 09:55:15 -0500

    Configured Sign Up + Profile UI

---
+++ Cigar'D/Cigar'D/Cigar'D/View/SignUpViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignUpViewController.swift
+//  Cigar'D
+//
+//  Created by Austin Potts on 12/16/21.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var userImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    @IBAction func registerButton(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
